c96932bbc7350288bce40708fa519aa343a688fe
doh-yo <doyo159090@gmail.com>
2024-06-07 18:31:45 +0900

    Feat: Addboard 컴포넌트 추가

--- api/itemApi.ts
+++ api/itemApi.ts
@@ -77,3 +77,34 @@ export async function getProductComments({
     throw error;
   }
 }
+
+export async function getArticleComments({
+  articleId,
+  limit = 10,
+}: {
+  articleId: number;
+  limit?: number;
+}) {
+  if (!articleId) {
+    throw new Error("Invalid article ID");
+  }
+
+  const params = {
+    limit: String(limit),
+  };
+
+  try {
+    const query = new URLSearchParams(params).toString();
+    const response = await fetch(
+      `https://panda-market-api.vercel.app/articles/${articleId}/comments?${query}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error("Failed to fetch article comments:", error);
+    throw error;
+  }
+}
